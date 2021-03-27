9fa03bac3770ad0a939cf67ab06c2d64db3a2e99
vcokltfre <vcokltfre@gmail.com>
2021-03-27 21:50:02 +0000

    feat: add timed cache util

---
+++ src/utils/cache.py
@@ -0,0 +1,24 @@
+from time import time
+
+
+class TimedCache:
+    def __init__(self, expire: int = 15):
+        self.items = {}
+        self.expire = expire
+
+    def __setitem__(self, key, value):
+        self.items[key] = {
+            "value": value,
+            "time": time(),
+        }
+
+    def __getitem__(self, key):
+        item = self.items.get(key)
+
+        if not item:
+            return None
+
+        if item["time"] + self.expire < time():
+            return None
+
+        return item["value"]
