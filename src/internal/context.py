5759f7fcda68c503122357d3d23463e4a504dd36
vcokltfre <vcokltfre@gmail.com>
2021-08-18 01:32:31 +0100

    feat: add thread methods to context

--- src/internal/context.py
+++ src/internal/context.py
@@ -1,7 +1,27 @@
+from discord import Message, Thread, TextChannel, ChannelType
+from discord.abc import Snowflake
 from discord.ext.commands import Context as _BaseContext
 
 
 class Context(_BaseContext):
     """A Custom Context for extra functionality."""
 
-    pass # Use to extend context functionality
+    async def create_message_thread(self, name: str, *, auto_archive_duration: int = 1440) -> Thread:
+        self.message: Message
+        return await self.message.create_thread(name=name, auto_archive_duration=auto_archive_duration)
+
+    async def create_channel_thread(
+        self,
+        name: str,
+        *,
+        message: Snowflake = None,
+        auto_archive_duration: int = 1440,
+        private: bool = False,
+    ) -> Thread:
+        self.channel: TextChannel
+        return await self.channel.create_thread(
+            name=name,
+            message=message,
+            auto_archive_duration=auto_archive_duration,
+            type=ChannelType.private if private else ChannelType.public,
+        )
